40a2c2ef5ae34fbd58ba84426e36793b403c4b5e
shoney.arickathil <shoney.arickathil@asellerat.com>
2025-07-18 12:47:12 +0200

    add language server and telescope
    well addding telescope and grepping the word on cursor is helpful

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,29 @@
+-- ~/.config/nvim/lua/plugins/cmp.lua
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "saadparwaiz1/cmp_luasnip",
+      "L3MON4D3/LuaSnip",
+      "rafamadriz/friendly-snippets",
+    },
+
+    opts = function(_, opts)
+      local cmp = require("cmp")
+
+      -- Extend/override default sources
+      opts.sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "buffer" },
+        { name = "path" },
+      })
+
+      return opts
+    end,
+  },
+}
