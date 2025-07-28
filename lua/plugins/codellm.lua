f6f19dd9c8ae5e001e05eb96becba5d76f4d2b45
shoney.arickathil <shoney.arickathil@asellerat.com>
2025-07-28 02:28:12 +0200

    add code companion to use local ollama llm

---
+++ lua/plugins/codellm.lua
@@ -0,0 +1,40 @@
+return {
+  "olimorris/codecompanion.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  opts = {
+    strategies = {
+      -- Change the default chat adapter
+      chat = {
+        adapter = "qwen",
+        inline = "qwen",
+      },
+    },
+    adapters = {
+      qwen = function()
+        return require("codecompanion.adapters").extend("ollama", {
+          name = "qwen", -- Give this adapter a different name to differentiate it from the default ollama adapter
+          schema = {
+            model = {
+              default = "qwen2.5-coder:7b",
+            },
+          },
+        })
+      end,
+    },
+
+    log_level = "DEBUG",
+
+    display = {
+      diff = {
+        enabled = true,
+        close_chat_at = 240, -- Close an open chat buffer if the total columns of your display are less than...
+        layout = "vertical", -- vertical|horizontal split for default provider
+        opts = { "internal", "filler", "closeoff", "algorithm:patience", "followwrap", "linematch:120" },
+        provider = "default", -- default|mini_diff
+      },
+    },
+  },
+}
